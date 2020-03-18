3da91d75c8e4472ab386e43fbb58e096c50418c0
Lionel Savidan <l.savidan.ubiqum@gmail.com>
2020-03-18 13:52:06 +0100

    salvo controller

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -1,4 +1,45 @@
 package com.codeoftheweb.salvo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
 public class SalvoController {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Autowired
+    private GamePlayerRepository gamePlayerRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+
+    @RequestMapping("/games")
+    public List<Map<String,Object>> getAllGames() {
+        return gameRepository
+                .findAll()
+                .stream()
+                .map(game -> makeGamesDTO(game))
+                .collect(Collectors.toList());
+    }
+
+    private Map<String, Object> makeGamesDTO(Game game) {
+        Map<String, Object> dto = new LinkedHashMap<String, Object>();
+        dto.put("game_id", game.getGameId());
+        dto.put("game_creation_date", game.getGameCreationDate());
+
+        return dto;
+    }
+
+
+
 }
