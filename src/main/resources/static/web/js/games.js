b852335fc683914dbf74e65ab26a91e833c26a51
Lionel Savidan <l.savidan.ubiqum@gmail.com>
2020-03-19 13:12:09 +0100

    fetch done

--- src/main/resources/static/web/js/games.js
+++ src/main/resources/static/web/js/games.js
@@ -1,1 +1,15 @@
-$(document).ready(function)
+fetch("http://localhost:8080/api/games"
+
+    ).then(function (response) {
+    return response.json();
+
+}).then((dataFromServer) => {
+    console.log(dataFromServer);
+
+
+})
+
+function showListGames(){
+
+document.getElementById('attendance-data').innerHTML +=
+}
